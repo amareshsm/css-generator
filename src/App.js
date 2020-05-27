6a2288c74862e4b963bb0276db85d02e0dcd6954
amareshsm <amareshsm13@gmail.com>
2020-05-27 14:42:58 +0530

    bootrstrap css added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BorderRadius from "./Components/BorderRadius.js";
 import TextShadow from "./Components/TextShadow";
 import FlexBoxGenerator from "./Components/FlexBoxGenerator";
 import Margin from "./Components/Margin";
+import { Button, Navbar,Nav,Form,NavDropdown,FormControl} from 'react-bootstrap';
+
 
 import { Switch, BrowserRouter, Route, Link } from "react-router-dom";
 
@@ -12,17 +14,35 @@ class App extends Component {
   render() {
     return (
       <>
-        <BrowserRouter>
-          <a href="/BoxShadow">BoxShadow</a>
-          <br></br>
-          <a href="/BorderRadius">BorderRadius</a>
-          <br></br>
-          <a href="/TextShadow">TextShadow</a>
-          <br></br>
-          <a href="/FlexBoxGenerator">Flex Box Generator</a>
-          <br></br>
-          <a href="/Margin">Margin and Padding</a>
 
+          
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/BoxShadow">Box Shadow</Nav.Link>
+      <NavDropdown title="css generators" id="basic-nav-dropdown">
+        <NavDropdown.Item href="/BoxShadow">BoxShadow</NavDropdown.Item>
+        <NavDropdown.Item  href="/BorderRadius">BorderRadius</NavDropdown.Item>
+        <NavDropdown.Item href="/TextShadow">TextShadow</NavDropdown.Item>
+        <NavDropdown.Item href="/FlexBoxGenerator">Flex Box Generator</NavDropdown.Item>
+        <NavDropdown.Item href="/Margin">Margin and Padding</NavDropdown.Item>
+
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+
+
+<BrowserRouter>
           <Switch>
             {/* <Route path="/" component={Home} exact />*/}
             <Route path="/BoxShadow" component={BoxShadow} />
@@ -31,7 +51,9 @@ class App extends Component {
             <Route path="/FlexBoxGenerator" component={FlexBoxGenerator} />
             <Route path="/Margin" component={Margin} />
           </Switch>
-        </BrowserRouter>
+        </BrowserRouter>      
+
+
       </>
     );
   }
