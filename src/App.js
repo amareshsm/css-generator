e927cf43dc4360dd5f5cb80471c83e27ce0487a3
DeepshikaS <ddeepu13@gmail.com>
2020-06-23 22:55:04 +0530

    FontSize

--- src/App.js
+++ src/App.js
@@ -20,137 +20,143 @@ import WordBreak from "./Components/WordBreak";
 import TextOverflow from "./Components/TextOverflow";
 import CssFloat from "./Components/CssFloat";
 import Overflow from "./Components/Overflow";
+import FontSize from "./Components/FontSize";
 import { Switch, BrowserRouter, Route, Link } from "react-router-dom";
-
 
 class App extends Component {
   componentDidMount() {
-// Mobile
-var menulist = document.getElementById("menu-list");
-var sublist = document.getElementById('sub-menu');
+    // Mobile
+    var menulist = document.getElementById("menu-list");
+    var sublist = document.getElementById("sub-menu");
 
-document.querySelector('.mobile-menu-open').addEventListener('click',openMenu);
+    document
+      .querySelector(".mobile-menu-open")
+      .addEventListener("click", openMenu);
 
-function openMenu(){
-  showAndHide(menulist);
-}
-document.getElementById('dropdown').onclick = function(){
-  showAndHide(sublist);
-}
+    function openMenu() {
+      showAndHide(menulist);
+    }
+    document.getElementById("dropdown").onclick = function () {
+      showAndHide(sublist);
+    };
 
-function showAndHide(obj){
-  if(window.innerWidth > 799) return;
-  if(obj.classList.contains('shown')){
-    obj.classList.add('hidden');
-    obj.classList.remove('shown');
-    return;
+    function showAndHide(obj) {
+      if (window.innerWidth > 799) return;
+      if (obj.classList.contains("shown")) {
+        obj.classList.add("hidden");
+        obj.classList.remove("shown");
+        return;
+      }
+      obj.classList.remove("hidden");
+      obj.classList.add("shown");
+    }
+
+    //Resize the window - change desktop or mobile version
+    document.body.onresize = function () {
+      if (window.innerWidth > 799) {
+        menulist.classList.remove("hidden");
+        menulist.classList.remove("shown");
+        sublist.classList.remove("hidden");
+        sublist.classList.remove("shown");
+      }
+    };
   }
-  obj.classList.remove('hidden');
-  obj.classList.add('shown');
-}
-
-
-
-//Resize the window - change desktop or mobile version
-document.body.onresize = function(){
-  if(window.innerWidth > 799){
-    menulist.classList.remove('hidden');
-    menulist.classList.remove('shown');
-    sublist.classList.remove('hidden');
-    sublist.classList.remove('shown');
-  }
-}
-  }
-
-
-
 
   render() {
     return (
       <>
-    
-<div class="navbar-wrapper">
-  <span class="mobile-menu-open" id="menu-open">
-    <hr /><hr /><hr />
-  </span>
-  <ul class="navbar-menu-list" id="menu-list">
+        <div class="navbar-wrapper">
+          <span class="mobile-menu-open" id="menu-open">
+            <hr />
+            <hr />
+            <hr />
+          </span>
+          <ul class="navbar-menu-list" id="menu-list">
+            <li>
+              <span>
+                {" "}
+                <a href="/">Home</a>
+              </span>
+            </li>
+            <li>
+              <span>CSS </span>
+            </li>
+            <li id="dropdown">
+              <span>DROP DOWN</span>
+              <ul class="sub-menu" id="sub-menu">
+                <li>sub menu 1</li>
+                <li>sub menu 2</li>
+                <li>sub menu 3</li>
+              </ul>
+            </li>
+            <li>
+              <span>Contact</span>
+            </li>
+          </ul>
+        </div>
 
-    <li><span>  <a href="/">Home</a></span></li>
-    <li><span>CSS </span></li>
-    <li id="dropdown">
-      <span>DROP DOWN</span>
-      <ul class="sub-menu" id="sub-menu">
-        <li>sub menu 1</li>
-        <li>sub menu 2</li>
-        <li>sub menu 3</li>
-      </ul>
-    </li>
-    <li><span>Contact</span></li>
-  </ul>
-</div>
-
-
-
-                  <ul className="nav-dropdown">
-                    <li>
-                      <a href="CssFloat">CssFloat</a>
-                    </li>
-                    <li>
-                      <a href="TextOverflow">TextOverflow</a>
-                    </li>
-                    <li>
-                      <a href="WordWrap">WordWrap</a>
-                    </li>
-                    <li>
-                      <a href="Overflow">Overflow</a>
-                    </li>
-                    <li>
-                      <a href="BoxShadow">BoxShadow</a>
-                    </li>
-                    <li>
-                      <a href="BorderRadius">BorderRadius</a>
-                    </li>
-                    <li>
-                      <a href="TextShadow">TextShadow</a>
-                    </li>
-                    <li>
-                      <a href="FlexBoxGenerator">FlexBoxGenerator</a>
-                    </li>
-                    <li>
-                      <a href="Margin">Margin and Padding</a>
-                    </li>
-                    <li>
-                      <a href="LineHeight">LineHeight</a>
-                    </li>
-                    <li>
-                      <a href="FontStyle">FontStyle</a>
-                    </li>
-                    <li>
-                      <a href="LetterSpacing">LetterSpacing</a>
-                    </li>
-                    <li>
-                      <a href="TextTransform">TextTransform</a>
-                    </li>
-                    <li>
-                      <a href="TextDecoration">TextDecoration</a>
-                    </li>
-                    <li>
-                      <a href="TextAlign">TextAlign</a>
-                    </li>
-                    <li>
-                      <a href="Outline">Outline</a>
-                    </li>
-                    <li>
-                      <a href="ListStyle">ListStyle</a>
-                    </li>
-                    <li>
-                      <a href="WritingMode">WritingMode</a>
-                    </li>
-                    <li>
-                      <a href="WordBreak">WordBreak</a>
-                    </li>
-                  </ul>
-
+        <ul className="nav-dropdown">
+          <li>
+            <a href="FontSize">FontSize</a>
+          </li>
+          <li>
+            <a href="CssFloat">CssFloat</a>
+          </li>
+          <li>
+            <a href="TextOverflow">TextOverflow</a>
+          </li>
+          <li>
+            <a href="WordWrap">WordWrap</a>
+          </li>
+          <li>
+            <a href="Overflow">Overflow</a>
+          </li>
+          <li>
+            <a href="BoxShadow">BoxShadow</a>
+          </li>
+          <li>
+            <a href="BorderRadius">BorderRadius</a>
+          </li>
+          <li>
+            <a href="TextShadow">TextShadow</a>
+          </li>
+          <li>
+            <a href="FlexBoxGenerator">FlexBoxGenerator</a>
+          </li>
+          <li>
+            <a href="Margin">Margin and Padding</a>
+          </li>
+          <li>
+            <a href="LineHeight">LineHeight</a>
+          </li>
+          <li>
+            <a href="FontStyle">FontStyle</a>
+          </li>
+          <li>
+            <a href="LetterSpacing">LetterSpacing</a>
+          </li>
+          <li>
+            <a href="TextTransform">TextTransform</a>
+          </li>
+          <li>
+            <a href="TextDecoration">TextDecoration</a>
+          </li>
+          <li>
+            <a href="TextAlign">TextAlign</a>
+          </li>
+          <li>
+            <a href="Outline">Outline</a>
+          </li>
+          <li>
+            <a href="ListStyle">ListStyle</a>
+          </li>
+          <li>
+            <a href="WritingMode">WritingMode</a>
+          </li>
+          <li>
+            <a href="WordBreak">WordBreak</a>
+          </li>
+        </ul>
 
         <BrowserRouter>
           <Switch>
@@ -174,6 +180,7 @@ document.body.onresize = function(){
             <Route path="/TextOverflow" component={TextOverflow} />
             <Route path="/Overflow" component={Overflow} />
             <Route path="/WordWrap" component={WordWrap} />
+            <Route path="/FontSize" component={FontSize} />
           </Switch>
         </BrowserRouter>
       </>
---
+++ src/Components/FontSize.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "../Styles/FontSize.css";
+
+class FontSize extends Component {
+  constructor(porps) {
+    super(porps);
+    this.state = {
+      Font_Size: "xx-small",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.FontSizeStyle = this.FontSizeStyle.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  async onChange(e) {
+    await this.setState({
+      [e.target.name]: e.target.value,
+    });
+    await this.FontSizeStyle();
+  }
+
+  async onSelect(e) {
+    await this.setState({
+      Font_Size: [e.target.value],
+    });
+    await this.FontSizeStyle();
+  }
+
+  FontSizeStyle() {
+    let e = document.querySelector(".FontSize");
+    e.style.fontSize = `${this.state.Font_Size}`;
+    console.log(this.state.Font_Size);
+    document.body.appendChild(e);
+  }
+
+  componentDidMount() {
+    this.FontSizeStyle();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <label htmlFor="value">Font_Size</label>
+        <select
+          value={this.state.Font_Size}
+          name="Font_Size"
+          onChange={this.onSelect}
+        >
+          <option value="xx-small">xx-small </option>
+          <option value="x-small">x-small</option>
+          <option value="small">small</option>
+          <option value="medium">Medium</option>
+          <option value="large">Large</option>
+          <option value="x-large">x-large</option>
+          <option value="xx-large">xx-large</option>
+          <option value="initial">Initial</option>
+          <option value="inherit">inherit</option>
+        </select>
+        <p></p>
+        <div className="FontSize">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget
+            consequat dui, et luctus quam. Aliquam magna libero, sodales non
+            eros nec, vestibulum sollicitudin leo. Quisque sed erat lorem.
+            Aliquam interdum ante purus, a blandit nunc placerat et. Proin
+            tristique ante tincidunt lacus dictum imperdiet. Quisque libero mi,
+            molestie et suscipit eget, convallis a ipsum.
+          </p>
+        </div>
+        <div> font-size = {this.state.Font_Size} ;</div>
+        <p></p>
+      </div>
+    );
+  }
+}
+
+export default FontSize;
