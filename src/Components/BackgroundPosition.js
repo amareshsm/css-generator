beff0c3d166f42fcaa458837bb4ccaceec57c9e0
DeepshikaS <ddeepu13@gmail.com>
2020-09-08 14:09:07 +0530

    Background Position

--- src/Components/BackgroundPosition.js
+++ src/Components/BackgroundPosition.js
@@ -57,10 +57,19 @@ class BackgroundPosition extends Component {
           <option value="center top">center top</option>
           <option value="center center">Center center</option>
           <option value="center bottom">center bottom</option>
+          <option value="initial">initial</option>
         </select>
         <p></p>
         <pre>
-          <div className="BackgroundPosition"></div>
+          <div className="BackgroundPosition">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
         </pre>
         <div> background-position = {BackgroundPosition} ;</div>
         <p></p>
